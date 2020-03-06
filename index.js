c38c53a1c0dc0f5774ca4de313588722cc53a281
Sebastian Menski <sebastian.menski@camunda.com>
2020-03-06 12:07:35 +0100

    debug

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const github = require('@actions/github');
 async function run() {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   core.debug(`The event name: ${github.context.eventName}`)
-  core.warning(`The event payload: ${payload}`);
-  core.debug(`The event name: ${github.context.eventName}`)
-  core.warning(`The event payload: ${payload}`);
+  core.debug(`The event payload: ${payload}`);
 }
 
 run().catch(error => core.setFailed(error.message));
